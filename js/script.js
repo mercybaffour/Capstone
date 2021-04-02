7b37dd235dc296fae32bc204607ce1ecadc6cfe2
Mercy Baffour <mercy.baffour@gmail.com>
2021-04-02 10:19:02 -0400

    Refactored code to async/await & fetch; incomplete

--- js/script.js
+++ js/script.js
@@ -13,52 +13,39 @@
 */
 
 
+
 //Event Listener on Download CSV Button
 function onClickCSV(){
-    let input = document.getElementById('target');
-    let url = "https://www.ebi.ac.uk/chembl/api/data/activity/search.json?limit=100&q=";
     let button = document.getElementById('csvbutton');
-    let parsed = [];
-    button.addEventListener('click', event => {
-        event.preventDefault();
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState === 4 && xhr.status === 200) {
-                parsed = JSON.parse(this.responseText);
-                console.log(parsed);
-                let activities = parsed.activities;
-                console.log(activities);
-                let filteredArray = filterArray(activities);
-                let jsonObject = classifyActivity(filteredArray);
-                console.log(jsonObject);
-                displayStructure(jsonObject);
-                let mdArray = toArrayOfArrays(jsonObject);
-                console.log(mdArray);
-                let dataset = ConvertToCSV(mdArray);
-                console.log(dataset);
-
-                //Browser allowing user to download CSV file
-                var a         = document.createElement('a');
-                a.href        = 'data:attachment/csv,' +  encodeURIComponent(dataset);
-                a.target      = '_blank';
-                a.download    = 'myFile.csv';
-
-                document.body.appendChild(a);
-                a.click();
-            }
-        }
-        xhr.open("GET", url + input.value);
-        xhr.send();
-    });
+    button.addEventListener('click', fetchData);
 }
 
 //Calling sequence of created functions that manipulate data from activity API on click of button.
 onClickCSV();
 
+function fetchData(){
+    let input = document.getElementById('target');
+    let url = "https://www.ebi.ac.uk/chembl/api/data/activity/search.json?limit=100&q=" + input.value;
+    return fetch(url)
+        .then(response => response.json())
+        .catch(err => console.log('failed', err))
+}
+
+async function manipulateData(){
+    let data = await fetchData();
+    console.log(data);
+    data.then((array) => {
+        console.log(array);
+        let activities = array.activities;
+        console.log(activities);
+        return activities
+    });
+}
+
 //Filter array with type = "IC50", and return new array with 4 relevant properties.
-function filterArray(arr){
-    let newArr = arr
-    .filter( (item) => {
+async function filterArray(){
+    let newArr = await manipulateData();
+    newArr.then(array => array.filter( (item) => {
         return item.type === "IC50";
     }).map( (item) => {
         return {
@@ -66,12 +53,13 @@ function filterArray(arr){
             canonical_smiles: item.canonical_smiles,
             standard_value: item.standard_value
         }
-    });
+    }));
     return newArr;
 }
 
 //Add bioactivity_class to array.
-function classifyActivity(arr){
+async function classifyActivity(){
+    let arr = await filterArray();
     for(let i=0; i<arr.length; i++){
         if(arr[i].standard_value >= 10000){
             arr[i].bioactivity_class = "inactive";
@@ -85,7 +73,8 @@ function classifyActivity(arr){
 }
 
 //Display list of molecules with their canonical smiles and link to structures
-function displayStructure(arr){
+async function displayStructure(){
+    let arr = await classifyActivity();
     let ul = document.createElement('ul');
     ul.style.listStyle = "none";
     let div = document.getElementById('renderList');
@@ -107,7 +96,8 @@ function displayStructure(arr){
 }
 
 //Array of Objects to Array of Arrays
-function toArrayOfArrays(input){
+async function toArrayOfArrays(){
+    let input = await classifyActivity();
     let output = input.map(function(obj) {
         return Object.keys(obj).sort().map(function(key) {
             return obj[key];
@@ -116,6 +106,25 @@ function toArrayOfArrays(input){
     return output;
 }
 
+//Convert to CSV format
+async function ConvertToCSV(){
+    let arr = await toArrayOfArrays();
+    let csvContent = "data:text/csv;charset=utf-8,"
+        + arr.map(e => e.join(",")).join("\n");
+    return csvContent;
+}
+
+async function downloadFile(){
+    let dataset = await ConvertToCSV();
+    var a = document.createElement('a');
+    a.href = 'data:attachment/csv,' +  encodeURIComponent(dataset);
+    a.target = '_blank';
+    a.download = 'myFile.csv';
+    document.body.appendChild(a);
+    a.click();
+}
+
+downloadFile();
 
 // //Duplicate Deletion
 // function deleteDuplicates(largeArray){
@@ -127,12 +136,3 @@ function toArrayOfArrays(input){
 //     console.log(uniqueArray);
 //     return uniqueArray;
 // }
-
-//Convert to CSV format
-function ConvertToCSV(arr){
-    let csvContent = "data:text/csv;charset=utf-8,"
-        + arr.map(e => e.join(",")).join("\n");
-    return csvContent;
-}
-
-
